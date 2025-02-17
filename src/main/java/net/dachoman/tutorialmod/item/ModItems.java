4a55300b11d826537ec30857f1c23638d86cfb7a
DaChoMan <meklowpa@gmail.com>
2025-02-16 22:12:57 -0800

    Added Pink Jewel and Raw Variant

---
+++ src/main/java/net/dachoman/tutorialmod/item/ModItems.java
@@ -0,0 +1,34 @@
+package net.dachoman.tutorialmod.item;
+
+import net.dachoman.tutorialmod.TutorialMod;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+
+    public static final Item PINK_JEWEL = registerItem("pink_jewel", new Item(new Item.Settings()));
+    public static final Item RAW_PINK_JEWEL = registerItem("raw_pink_jewel", new Item(new Item.Settings()));
+
+    private  static Item registerItem(String name, Item item) {
+
+        return Registry.register(Registries.ITEM, Identifier.of(TutorialMod.MOD_ID, name), item);
+
+    }
+
+    public static void registerModItems() {
+        TutorialMod.LOGGER.info("Registering Mod Items for " + TutorialMod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+
+            entries.add(PINK_JEWEL);
+            entries.add(RAW_PINK_JEWEL);
+
+        });
+
+    }
+
+}
